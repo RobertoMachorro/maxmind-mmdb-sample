0abb285929547f2361ddd23b6f409aca79bffa05
Roberto Machorro <7190436+RobertoMachorro@users.noreply.github.com>
2022-09-24 16:24:32 -0400

    Extracting elements from the MMDB record.

--- maxmind-mmdb-sample/main.swift
+++ maxmind-mmdb-sample/main.swift
@@ -14,4 +14,22 @@ let pathToMMDBFile = "YOURPATH/GeoLite2-Country.mmdb"
 let mmdbFileUrl = URL(fileURLWithPath: pathToMMDBFile)
 if let geolite = GeoLite2CountryDatabase(from: mmdbFileUrl) {
 	print(geolite.countryCode(address: testIP) ?? "not found")
+
+	switch geolite.search(address: testIP) {
+	case .notFound:
+		print("Data file not found.")
+	case .partial(_):
+		print("Partial.")
+	case .value(let v):
+		if case let .map(m) = v,
+		   case let .map(country) = m["country"],
+		   case let .string(country_isocode) = country["iso_code"],
+		   case let .map(country_names) = country["names"],
+		   case let .string(country_name) = country_names["en"]
+		{
+			print("country_isocode=\(country_isocode) country_name=\(country_name)")
+		}
+	case .failed(_):
+		print("Search failed.")
+	}
 }
